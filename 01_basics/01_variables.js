39649262b288da360e285a5495d3673ccf806be0
Pawan <59733142+PawanBastola@users.noreply.github.com>
2024-08-03 14:10:13 +0000

    variables and data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14456
+let accountEmail = "abc@google.com"
+var accountPasword = "123456"
+accountCity = "Jaipur"
+
+// accountId = 2
+accountEmail = "hc@hc.com"
+accountPassword = "12121"
+accountCity = "Bengaluru"
+let accountstate
+// prefer not to use var due to the isssue in scope and functional scope
+console.log(accountId);
+console.table([accountEmail,accountId,accountPasword,accountCity,accountstate]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version.
+//alert(3+3) // we are using node js, not browser.
+
+let name ="buddha";
+let age = 18;
+let isLoggedIn = false
+//number = > 2 to power 53
+//bigint // if large number
+//string => ""
+//boolean  => true/false
+// null => standalone value
+// undefined =>
+//symbol => unique
+
+
+// object
+console.log(typeof null);// null is an object
+console.log(typeof undefined);// undefined is of type undefined.
+
